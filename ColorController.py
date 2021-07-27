8e32d339e9d2f3ae42493c7ebd7bbd7600cc3c14
Tal Zaken <talzaken@gmail.com>
2021-07-27 17:35:22 -0400

    Added method to show color Added function to invert RGB color values Now raises a KeyError when a color name is not found upon setting a color by the name attribute (using query_hex_code). Added function to "find closes color name." Fixed many bugs in the getters/setters related to storing values in a list and specificity of values. See the unlist() function. Added methods to darken, lighten, and brighten a ColorController object.

--- ColorController.py
+++ ColorController.py
@@ -2,6 +2,7 @@ import colorsys
 from math import sin, cos, pi
 
 import pandas as pd
+from tkinter import Tk, Text, INSERT, END
 
 colors_df = pd.read_csv('colornames.txt', delimiter=" ", skiprows=60, header=None)
 colors_df.columns = ['IDX', 'NAME',
@@ -14,6 +15,22 @@ colors_df.columns = ['IDX', 'NAME',
                      'cmyk', 'C', 'M', 'Y', 'K',
                      'NEIGHBOUR_STR', 'NUM_NEIGHBOURS_MAXDE', 'WORD_TAGS']
 colors_df['WORD_TAGS'] = colors_df['WORD_TAGS'].apply(lambda x: x.split(":"))
+
+
+def invert_rgb(r, g, b):
+    r = 255 - r
+    g = 255 - g
+    b = 255 - b
+    return r, g, b
+
+
+def unlist(data):
+    if data == []:
+        return ''
+    elif type(data) == list:
+        return data[-1]
+    else:
+        return data
 
 
 def rgb_to_hex(red, green, blue):
@@ -73,8 +90,32 @@ def query_hex_code(token=str):
     Takes a token, and checks whether it is present in a pre-defined dictionary of color names.
     If the token is found, it returns the full range of hex codes that match the color name.
     """
-    token = token.replace(" ", "_")
-    return list(colors_df.query(f'NAME=="{token.lower()}"').HEX)
+    token = token.replace(" ", "_").lower()
+    if token in colors_df.NAME.values:
+        return list(colors_df.query(f'NAME=="{token.lower()}"').HEX)
+    else:
+        raise KeyError("Color name not found in colornames.txt")
+
+
+def find_closest_color_name(hex_str=str):
+    """Calculates distance between a given color hex code and every color in the colornames.txt database,
+    and returns the a list of the closest color names.
+    Only returns multiple results if there is a tie for the closest color.
+    """
+    r, g, b = hex_to_rgb(hex_str)
+    h1, s1, v1 = colorsys.rgb_to_hsv(r, g, b)
+    h1, s1, v1 = colorsys_hsv_to_hsv360((h1, s1, v1))
+    h1, s1, v1 = hsv360_to_hsvdistance((h1, s1, v1))
+
+    closenames_df = pd.DataFrame(colors_df)
+    closenames_df['hsv'] = [(h, s, v) for h, s, v in
+                            zip(closenames_df['h'], closenames_df['s'], closenames_df['v'])]
+    closenames_df['hsv'] = closenames_df['hsv'].apply(hsv360_to_hsvdistance)
+    closenames_df['distance'] = closenames_df['hsv'].apply(lambda x: measure_hsv_distance((h1, s1, v1), x))
+
+    closest_distance = closenames_df.sort_values(by='distance').iloc[0].distance
+
+    return list(set(closenames_df[closenames_df['distance'] == closest_distance].NAME))
 
 
 class ColorController:
@@ -90,20 +131,22 @@ class ColorController:
         if self._name:
             return self._name
         elif self._hex_code:
-            self._name = self.find_closest_color_name(self._hex_code)
+            self._name = find_closest_color_name(self._hex_code)
             return self._name
         elif self._rgb:
             r, g, b = self._rgb
-            self._name = self.find_closest_color_name(rgb_to_hex(r, g, b))
+            hex_code = rgb_to_hex(r, g, b)
+            self._name = find_closest_color_name(hex_code)
             return self._name
         elif self._hsv:
             h, s, v = self._hsv
             r, g, b = colorsys.hsv_to_rgb(h, s, v)
-            self._name = self.find_closest_color_name(rgb_to_hex(r, g, b))
+            self._name = find_closest_color_name(rgb_to_hex(r, g, b))
             return self._name
 
     @name.setter
     def name(self, new_name):
+        print("name setter")
         self._name = new_name
         self._hex_code = query_hex_code(new_name)
         self._rgb = hex_to_rgb(self._hex_code[-1])
@@ -114,9 +157,6 @@ class ColorController:
     def hex_code(self):
         if self._hex_code:
             return self._hex_code
-        elif self._name:
-            self._hex_code = query_hex_code(self._name)
-            return self._hex_code
         elif self._rgb:
             r, g, b = self._rgb
             self._hex_code = rgb_to_hex(r, g, b)
@@ -126,22 +166,21 @@ class ColorController:
             r, g, b = colorsys.hsv_to_rgb(h, s, v)
             self._hex_code = rgb_to_hex(r, g, b)
             return self._hex_code
+        elif self._name:
+            self._hex_code = query_hex_code(self._name)[-1]
+            return self._hex_code
 
     @hex_code.setter
     def hex_code(self, new_hex_code):
         self._hex_code = new_hex_code
-        self._name = self.find_closest_color_name(new_hex_code)
+        self._name = find_closest_color_name(new_hex_code)
         self._rgb = hex_to_rgb(new_hex_code)
         r, g, b = self._rgb
         self._hsv = colorsys.rgb_to_hsv(r, g, b)
 
-    # PICK UP HERE: Need to write getters and setters for rgb and hsv
     @property
     def rgb(self):
         if self._rgb:
-            return self._rgb
-        elif self._name:
-            self._rgb = hex_to_rgb(query_hex_code(self._name))
             return self._rgb
         elif self._hex_code:
             self._rgb = hex_to_rgb(self._hex_code)
@@ -150,22 +189,21 @@ class ColorController:
             h, s, v = self._hsv
             self._rgb = colorsys.hsv_to_rgb(h, s, v)
             return self._rgb
+        elif self._name:
+            self._rgb = hex_to_rgb(unlist(query_hex_code(unlist(self._name))))
+            return self._rgb
 
     @rgb.setter
     def rgb(self, new_rgb):
         self._rgb = new_rgb
-        self._name = self.find_closest_color_name(rgb_to_hex(new_rgb))
-        self._hex_code = rgb_to_hex(new_rgb)
         r, g, b = self._rgb
+        self._name = find_closest_color_name(rgb_to_hex(r, g, b))
+        self._hex_code = rgb_to_hex(r, g, b)
         self._hsv = colorsys.rgb_to_hsv(r, g, b)
 
     @property
     def hsv(self):
         if self._hsv:
-            return self._hsv
-        elif self._name:
-            r, g, b = hex_to_rgb(query_hex_code(self._name[-1])[-1])
-            self._hsv = colorsys.rgb_to_hsv(r, g, b)
             return self._hsv
         elif self._hex_code:
             r, g, b = hex_to_rgb(self._hex_code)
@@ -175,32 +213,76 @@ class ColorController:
             r, g, b = self._rgb
             self._hsv = colorsys.rgb_to_hsv(r, g, b)
             return self._hsv
+        elif self._name:
+            r, g, b = hex_to_rgb(unlist(query_hex_code(unlist(self._name))))
+            self._hsv = colorsys.rgb_to_hsv(r, g, b)
+            return self._hsv
 
     @hsv.setter
     def hsv(self, new_hsv):
         self._hsv = new_hsv
         h, s, v = new_hsv
-        self._name = self.find_closest_color_name(rgb_to_hex(colorsys.hsv_to_rgb(h, s, v)))
-        self._hex_code = rgb_to_hex(colorsys.hsv_to_rgb(h, s, v))
+        r, g, b = colorsys.hsv_to_rgb(h, s, v)
+        self._name = find_closest_color_name(rgb_to_hex(r, g, b))
+        self._hex_code = rgb_to_hex(r, g, b)
         self._rgb = colorsys.hsv_to_rgb(h, s, v)
 
-    def find_closest_color_name(self, hex_str=str):
-        r, g, b = hex_to_rgb(hex_str)
-        h1, s1, v1 = colorsys.rgb_to_hsv(r, g, b)
-        h1, s1, v1 = colorsys_hsv_to_hsv360((h1, s1, v1))
-        h1, s1, v1 = hsv360_to_hsvdistance((h1, s1, v1))
+    def show_color(self):
+        """
+        Takes a properly-formatted hex color code as input,
+        and opens a window displaying the color.
+        """
 
-        closenames_df = pd.DataFrame(colors_df)
-        closenames_df['hsv'] = [(h, s, v) for h, s, v in
-                                zip(closenames_df['h'], closenames_df['s'], closenames_df['v'])]
-        closenames_df['hsv'] = closenames_df['hsv'].apply(hsv360_to_hsvdistance)
-        closenames_df['distance'] = closenames_df['hsv'].apply(lambda x: measure_hsv_distance((h1, s1, v1), x))
+        hc = '#' + unlist(self.hex_code).lstrip('#')
+        r_text, g_text, b_text = hex_to_rgb(hc)
+        r_text, g_text, b_text = invert_rgb(r_text, g_text, b_text)
+        text_color = rgb_to_hex(r_text, g_text, b_text)
+        gui = Tk(className=f' Hex Color Code: {unlist(hc)}; Color Name: {unlist(self.name)} ')
+        text = Text(gui, bg=f"{hc}", fg=text_color)
+        text.insert(INSERT, f' Hex Color Code: {hc}\n')
+        text.insert(INSERT, f' Color Name(s): {self.name}\n')
+        text.insert(INSERT, f' RGB Triplet: {self.rgb}\n')
+        text.insert(INSERT, f' HSV Triplet: {self.hsv}')
 
-        closest_distance = closenames_df.sort_values(by='distance').iloc[0].distance
+        text.pack()
+        gui.geometry("400x200")
+        gui.mainloop()
 
-        return list(set(closenames_df[closenames_df['distance'] == closest_distance].NAME))
+    def darken_color(self, darkening_value=.25):
+        """
+        Takes a hex code and returns a hex code for a darker shade of the original hex code.
+        Takes "darkening_value" as an optional input. Darkening value is a float between 0 and 1.
+        """
+        print("darkening color")
+        h, s, v = self.hsv
+        v = v * (1 - darkening_value)
+        self.hsv = (h, s, v)
+
+    def lighten_color(self, lightening_value=.25):
+        """
+        Takes a hex code and returns a hex code for a lighter shade of the original hex code.
+        Takes "lightening_value" as an optional input. Lightening value is a float between 0 and 1.
+        """
+        print("lightening color")
+        h, s, v = self.hsv
+        s = s * (1 - lightening_value)
+        self.hsv = (h, s, v)
+
+    def brighten_color(self, brightening_value=.25):
+        """
+        Takes a hex code and returns a hex code for a brighter shade of the original hex code.
+        Takes "brightening_value" as an optional input. Brightening value is a float between 0 and 1.
+        """
+        print("brightening color")
+        h, s, v = self.hsv
+        s = min((s + ((1 - s) * brightening_value)), 1)
+        v = min((v * (1 + brightening_value), 255))
+        self.hsv = (h, s, v)
 
 
-a = ColorController(rgb=(10, 30, 100))
-print(a.hsv, a.name)
-
+a = ColorController(name='yellow')
+a.show_color()
+a.lighten_color()
+a.show_color()
+a.brighten_color(1)
+a.show_color()
