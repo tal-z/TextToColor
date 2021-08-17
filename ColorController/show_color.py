01ebe1b5956bafd8bf2ba279935d913e06ad43a4
Tal Zaken <talzaken@gmail.com>
2021-08-16 21:06:56 -0400

    added method for showing a coded color, imporved names

--- ColorController/show_color.py
+++ ColorController/show_color.py
@@ -1,5 +1,6 @@
 #from ColorController import ColorController, colors_df
 import matplotlib.pyplot as plt
+from conversions import invert_rgb, rgb_to_hex
 
 
 def place_title_linebreak(title):
@@ -24,11 +25,25 @@ def set_title_fontsize(title):
     return fontsize
 
 
-def show_namedcolor(color_object):
+def show_named_color(color_object):
     data = [1 for code in color_object.hex_code]
     explode = [.05 for d in data]
     plt.pie(data, explode=explode, labels=color_object.hex_code, colors=color_object.hex_code)
     plt.title(place_title_linebreak(color_object.name.upper()), fontsize=set_title_fontsize(color_object.name))
     plt.tight_layout()
+    plt.show()
+
+
+def show_coded_color(color_object):
+    data = [1 for name in color_object.name]
+    plt.pie(data, labels=color_object.name, colors=[color_object.hex_code for name in color_object.name])
+    r, g, b = color_object.rgb
+    text_r, text_g, text_b = invert_rgb(r, g, b)
+    text_color = rgb_to_hex(text_r, text_g, text_b)
+
+    plt.text(x=-.65, y=-.15, s=f'Hex Code: {color_object.hex_code}\n'
+                             f'RGB: {color_object.rgb}\n'
+                             f'HSV: {color_object.hsv}', color=text_color)
+    #plt.title(place_title_linebreak(color_object.name.upper()), fontsize=set_title_fontsize(color_object.name))
     plt.show()
 
