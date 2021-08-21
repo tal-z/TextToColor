7909d47814da8347c9d218e80cf796f65709d250
Tal Zaken <talzaken@gmail.com>
2021-08-21 14:58:37 -0400

    added extra assertion to test case

--- ColorController/test_conversions.py
+++ ColorController/test_conversions.py
@@ -6,9 +6,10 @@ class TestRgbToHex(unittest.TestCase):
         self.assertEqual(rgb_to_hex(5, 30, 255), '#051eff')
         self.assertEqual(rgb_to_hex(5.0, 29.9999, 255), '#051eff')
         self.assertEqual(rgb_to_hex(5.0, 30.0001, 255), '#051eff')
+        self.assertRaises(ValueError, rgb_to_hex, 200, -30, 100)
         with self.assertRaises(ValueError) as context:
             rgb_to_hex(500, 30.1, 255.01)
-        self.assertTrue('Inputs for red, green, and blue must be integer values between 0 and 255.' in str(context.exception))
-        with self.assertRaises(ValueError) as context:
-            rgb_to_hex(200, -30, 100)
-        self.assertTrue('Inputs for red, green, and blue must be integer values between 0 and 255.' in str(context.exception))
+        self.assertTrue(
+            'Inputs for red, green, and blue must be integer values between 0 and 255.'
+            in str(context.exception)
+        )
