baf454b936483480d920bfcb8475445e8a059488
Tal Zaken <talzaken@gmail.com>
2021-08-22 10:27:38 -0400

    Added custom round function to helpers, which breaks ties in the normal round-up way.

--- ColorController/helpers.py
+++ ColorController/helpers.py
@@ -1,3 +1,4 @@
+import math
 
 def unlist(data):
     if data == []:
@@ -6,3 +7,15 @@ def unlist(data):
         return data[-1]
     else:
         return data
+
+
+def hsv_round(n, decimals=0):
+    """
+    Custom round function to return a value rounded to the correct number of decimals.
+    In the case of HSV360, this is zero decimals (integer).
+    This custom round function works differently than the built-in round() function. It breaks ties by rounding up,
+    whereas the built-in round() function breaks ties by rounding to the nearest even number.
+    """
+    multiplier = 10 ** decimals
+    return math.floor(n*multiplier + 0.5) / multiplier
+
