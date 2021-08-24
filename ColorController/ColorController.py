5242d6be6b6ef279d26650c5b4a2970dddce836f
Tal Zaken <talzaken@gmail.com>
2021-08-23 21:02:11 -0400

    swapped out repetitive code for format_hsv() helper

--- ColorController/ColorController.py
+++ ColorController/ColorController.py
@@ -14,14 +14,14 @@ class ColorController:
             self._rgb = hex_to_rgb(hex_code)
             red, green, blue = self._rgb
             h, s, v = colorsys.rgb_to_hsv(red, green, blue)
-            self._hsv = (float('%.3g' % h), float('%.3g' % s), int(float('%.3g' % v)))
+            self._hsv = format_hsv(h, s, v)
             self._name = find_closest_color_names(hex_code)
         elif rgb:
             red, green, blue = rgb
             self._hex_code = rgb_to_hex(red, green, blue)
             self._rgb = rgb
             h, s, v = colorsys.rgb_to_hsv(red, green, blue)
-            self._hsv = (float('%.3g' % h), float('%.3g' % s), int(float('%.3g' % v)))
+            self._hsv = format_hsv(h, s, v)
             self._name = find_closest_color_names(self._hex_code)
         elif hsv:
             h, s, v = hsv
@@ -37,7 +37,7 @@ class ColorController:
             for color in self._rgb:
                 red, green, blue = color
                 h, s, v = colorsys.rgb_to_hsv(red, green, blue)
-                hsv_list.append((float('%.3g' % h), float('%.3g' % s), int(float('%.3g' % v))))
+                hsv_list.append(format_hsv(h, s, v))
             self._hsv = hsv_list
             self._name = name
 
@@ -45,8 +45,9 @@ class ColorController:
     def name(self):
         """
         Because I want to auto-update values as they are called, I have selected to use "getters and setters,"
-        implemented in Python as class properties. This is the "getter" method, and it's job is to return
-        the .name property. It works by  checking to see if there is a name set in the ._name attribute,
+        implemented in Python as class properties. Class properties are themselves "decorators."
+        This is the "getter" method, and it's job is to return the .name property.
+        It works by  checking to see if there is a name set in the ._name attribute,
         and returns the name if there is. Because name is always set upon init, the check is redundant.
         """
         if self._name:
@@ -66,7 +67,7 @@ class ColorController:
         for color in self._rgb:
             red, green, blue = color
             h, s, v = colorsys.rgb_to_hsv(red, green, blue)
-            hsv_list.append((float('%.3g' % h), float('%.3g' % s), int(float('%.3g' % v))))
+            hsv_list.append(format_hsv(h, s, v))
         self._hsv = hsv_list
 
     @property
@@ -86,7 +87,7 @@ class ColorController:
         self._rgb = hex_to_rgb(new_hex_code)
         red, green, blue = self._rgb
         h, s, v = colorsys.rgb_to_hsv(red, green, blue)
-        self._hsv = (float('%.3g' % h), float('%.3g' % s), int(float('%.3g' % v)))
+        self._hsv = format_hsv(h, s, v)
 
     @property
     def rgb(self):
@@ -102,7 +103,7 @@ class ColorController:
         self._name = find_closest_color_names(rgb_to_hex(red, green, blue))
         self._hex_code = rgb_to_hex(red, green, blue)
         h, s, v = colorsys.rgb_to_hsv(red, green, blue)
-        self._hsv = (float('%.3g' % h), float('%.3g' % s), int(float('%.3g' % v)))
+        self._hsv = format_hsv(h, s, v)
 
     @property
     def hsv(self):
@@ -131,7 +132,7 @@ class ColorController:
         print("darkening color")
         h, s, v = unlist(self.hsv)
         v = v * (1 - darkening_value)
-        self.hsv = (float('%.3g' % h), float('%.3g' % s), int(float('%.3g' % v)))
+        self.hsv = format_hsv(h, s, v)
 
     def lighten_color(self, lightening_value=.25):
         """
@@ -141,8 +142,8 @@ class ColorController:
         print("lightening color")
         h, s, v = unlist(self.hsv)
         s = s * (1 - lightening_value)
-        v = min(v * (1+lightening_value) or 50, 255)
-        self.hsv = (float('%.3g' % h), float('%.3g' % s), int(float('%.3g' % v)))
+        v = min((v * (1+lightening_value)) or (255*(lightening_value/1)), 255)
+        self.hsv = format_hsv(h, s, v)
 
     def brighten_color(self, brightening_value=.25):
         """
@@ -153,7 +154,7 @@ class ColorController:
         h, s, v = unlist(self.hsv)
         s = min((s + ((1 - s) * brightening_value)), 1)
         v = min((v * (1 + brightening_value), 255))
-        self.hsv = (float('%.3g' % h), float('%.3g' % s), int(float('%.3g' % v)))
+        self.hsv = format_hsv(h, s, v)
 
     def show_color(self):
         if type(self.hex_code) == list:
@@ -163,24 +164,5 @@ class ColorController:
 
 
 if __name__ == '__main__':
-    print(colors_df.NAME.value_counts())
-    c = ColorController(name='maroon')
-    c.show_color()
-    c.hex_code = c.hex_code[-3]
-    c.show_color()
-    c.lighten_color()
-    c.show_color()
-    c.lighten_color()
-    c.show_color()
-    c.lighten_color()
-    c.show_color()
-    c.lighten_color()
-    c.show_color()
-    c.lighten_color()
-    c.show_color()
-    c.lighten_color()
-    c.show_color()
-    c.lighten_color()
-    c.show_color()
-    c.lighten_color()
+    c = ColorController(hex_code='#ff0055')
     c.show_color()
