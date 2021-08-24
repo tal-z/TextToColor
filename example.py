35881e5f59d8403c4d1918ad4770f68e24c620c2
Tal Zaken <talzaken@gmail.com>
2021-08-24 18:24:03 -0400

    don't worry about the example. it's a playground for human testing.

--- example.py
+++ example.py
@@ -7,11 +7,8 @@ def mix_rgb(rgb1, rgb2):
     return tuple(int(regular_round(sum([ch1, ch2]) / 2)) for (ch1, ch2) in zip(rgb1, rgb2))
 
 
-c1 = ColorController(hex_code='ff0000')
-
+c1 = ColorController(name='maroon')
 c1.show_color()
-c2 = ColorController(hex_code='00ffff')
-
-c2.show_color()
-color = ColorController(rgb=mix_rgb(c1.rgb, c2.rgb))
-color.show_color()
+for i in range(10):
+    c1.hex_code = str(int('193123') + 1000*i)
+    c1.show_color()
