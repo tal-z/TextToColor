1e5e277926a8fd1af61eebbb6634a73d724af583
Tal Zaken <talzaken@gmail.com>
2021-08-24 17:44:27 -0400

    minor fix to toy color mixing function, which demonstrates the difficulties of mixing rgb values.

--- example.py
+++ example.py
@@ -2,10 +2,8 @@ from ColorController import ColorController
 from ColorController.helpers import regular_round
 
 
-def mix_rgb(rgb1, rgb2):
-    r1, g1, b1 = rgb1
-    r2, g2, b2 = rgb2
 
+def mix_rgb(rgb1, rgb2):
     return tuple(int(regular_round(sum([ch1, ch2]) / 2)) for (ch1, ch2) in zip(rgb1, rgb2))
 
 
