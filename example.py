a1620ad93c3e1ad2034bcb4e51e12f0d2a3d1961
Tal Zaken <talzaken@gmail.com>
2021-08-24 17:07:51 -0400

    Added example.py, which will be my testing file and eventually host code snippets of sample usages.

---
+++ example.py
@@ -0,0 +1,19 @@
+from ColorController import ColorController
+from ColorController.helpers import regular_round
+
+
+def mix_rgb(rgb1, rgb2):
+    r1, g1, b1 = rgb1
+    r2, g2, b2 = rgb2
+
+    return tuple(int(regular_round(sum([ch1, ch2]) / 2)) for (ch1, ch2) in zip(rgb1, rgb2))
+
+
+c1 = ColorController(hex_code='ff0000')
+
+c1.show_color()
+c2 = ColorController(hex_code='00ffff')
+
+c2.show_color()
+color = ColorController(rgb=mix_rgb(c1.rgb, c2.rgb))
+color.show_color()
