597300bc9abd863cb852ceea862eec735dd72bba
Tal Zaken <talzaken@gmail.com>
2021-08-24 17:47:36 -0400

    Handled text color setting on show_color for coded color, when invert_rgb produces a value too similar to the non-inverted value

--- ColorController/show_color.py
+++ ColorController/show_color.py
@@ -62,7 +62,11 @@ def show_coded_color(color_object):
     plt.title(color_object.hex_code.upper(), fontsize=40)
     r, g, b = color_object.rgb
     text_r, text_g, text_b = invert_rgb(r, g, b)
-    text_color = rgb_to_hex(text_r, text_g, text_b)
+    print((r, text_r), (g, text_g), (b, text_b), sep='\n')
+    if any(abs(ch1-ch2) < 4 for (ch1, ch2) in zip((text_r, text_g, text_b), (r, g, b))):
+        text_color = '#000000'
+    else:
+        text_color = rgb_to_hex(text_r, text_g, text_b)
     ax.text(x=-.65, y=-.15, s=f'Hex Code: {color_object.hex_code}\n'
                               f'RGB: {color_object.rgb}\n'
                               f'HSV: {color_object.hsv}', color=text_color)
