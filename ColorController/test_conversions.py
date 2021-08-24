fcb7f9102a97cccac5c1803a9c0c7db4d69f872b
Tal Zaken <talzaken@gmail.com>
2021-08-24 10:43:58 -0400

    re-ordered tests to match conversions.py module. added test cases for hsv360_to_hsvdistance conversion.

--- ColorController/test_conversions.py
+++ ColorController/test_conversions.py
@@ -1,35 +1,11 @@
 import colorsys
 import unittest
+from math import pi
 
-from conversions import rgb_to_hex, hex_to_rgb, invert_rgb, colorsys_hsv_to_hsv360
+from conversions import rgb_to_hex, hex_to_rgb, invert_rgb, colorsys_hsv_to_hsv360, hsv360_to_hsvdistance
 
 
 class TestConversions(unittest.TestCase):
-
-    def test_rgb_to_hex(self):
-        self.assertEqual(rgb_to_hex(0, 0, 0), '#000000')
-        self.assertEqual(rgb_to_hex(255, 255, 255), '#ffffff')
-        self.assertEqual(rgb_to_hex(0, 0, 1), '#000001')
-        self.assertEqual(rgb_to_hex(1, 1, 1), '#010101')
-        self.assertEqual(rgb_to_hex(5, 30, 255), '#051eff')
-        self.assertEqual(rgb_to_hex(5.0, 29.9999, 255), '#051eff')
-        self.assertEqual(rgb_to_hex(5.0, 30.0001, 255), '#051eff')
-        self.assertWarns(UserWarning, rgb_to_hex, 200.1, 20, 90.5)
-        self.assertRaises(ValueError, rgb_to_hex, 200, -30, 100)
-        with self.assertRaises(ValueError) as context:
-            rgb_to_hex(500, 30.1, 255.01)
-        self.assertTrue('Inputs for red, green, and blue must be '
-                        'integer values between 0 and 255.' in str(context.exception)
-                        )
-
-    def test_hex_to_rgb(self):
-        self.assertEqual(hex_to_rgb('#000000'), (0, 0, 0))
-        self.assertEqual(hex_to_rgb('#FFFFFF'), (255, 255, 255))
-        self.assertEqual(hex_to_rgb('0c2526'), (12, 37, 38))
-        self.assertEqual(hex_to_rgb('#85d849'), (133, 216, 73))
-        self.assertEqual(hex_to_rgb('2A918A'), (42, 145, 138))
-        self.assertRaises(ValueError, hex_to_rgb, '2A9181b')
-        self.assertRaises(ValueError, hex_to_rgb, '12345')
 
     def test_invert_rgb(self):
         self.assertEqual(invert_rgb(255, 255, 255), (0, 0, 0))
@@ -43,11 +19,37 @@ class TestConversions(unittest.TestCase):
                         f"You entered values (100, 255, -30) for (r, g, b)" in str(context.exception)
                         )
 
+    def test_rgb_to_hex(self):
+        self.assertEqual(rgb_to_hex(0, 0, 0), '#000000')
+        self.assertEqual(rgb_to_hex(255, 255, 255), '#ffffff')
+        self.assertEqual(rgb_to_hex(0, 0, 1), '#000001')
+        self.assertEqual(rgb_to_hex(1, 1, 1), '#010101')
+        self.assertEqual(rgb_to_hex(5, 30, 255), '#051eff')
+        self.assertEqual(rgb_to_hex(5.0, 29.9999, 255), '#051eff')
+        self.assertEqual(rgb_to_hex(5.0, 30.0001, 255), '#051eff')
+        self.assertWarns(UserWarning, rgb_to_hex, 200.1, 20, 90.5)
+        self.assertRaises(ValueError, rgb_to_hex, 200, -30, 100)
+        with self.assertRaises(ValueError) as context:
+            rgb_to_hex(500, 30.1, 255.01)
+        self.assertTrue(f"Inputs for red, green, and blue must be integer values between 0 and 255. "
+                        f"You entered values (500, 30.1, 255.01) for (red, green, blue)" in str(context.exception)
+                        )
+
+    def test_hex_to_rgb(self):
+        self.assertEqual(hex_to_rgb('#000000'), (0, 0, 0))
+        self.assertEqual(hex_to_rgb('#FFFFFF'), (255, 255, 255))
+        self.assertEqual(hex_to_rgb('0c2526'), (12, 37, 38))
+        self.assertEqual(hex_to_rgb('#85d849'), (133, 216, 73))
+        self.assertEqual(hex_to_rgb('2A918A'), (42, 145, 138))
+        self.assertRaises(ValueError, hex_to_rgb, '2A9181b')
+        with self.assertRaises(ValueError) as context:
+            hex_to_rgb('12345')
+        self.assertTrue('Hex code must be 6 digits long.' in str(context.exception))
 
     def test_colorsys_hsv_to_hsv360(self):
         self.assertEqual(colorsys_hsv_to_hsv360((0, 0, 255)), (0, 0, 100))
         self.assertEqual(colorsys_hsv_to_hsv360((1, 1, 255)), (360, 100, 100))
-        self.assertEqual(colorsys_hsv_to_hsv360((.25, .25, 100)), (90, 25, int(100/2.55)))
+        self.assertEqual(colorsys_hsv_to_hsv360((.25, .25, 100)), (90, 25, int(100 / 2.55)))
 
     def test_hex_to_hsv360(self):
         r, g, b = hex_to_rgb('#F4ED7C')
@@ -66,4 +68,14 @@ class TestConversions(unittest.TestCase):
         h, s, v = colorsys.rgb_to_hsv(r, g, b)
         h, s, v = colorsys_hsv_to_hsv360((h, s, v))
         self.assertEqual((h, s, v), (293, 51, 14))
+
+    def test_hsv360_to_hsvdistance(self):
+        """
+        Takes an HSV triplet as provided by colorsys_hsv_to_hsv360(), and converts it to match the
+        notation used in the function for calculating distance between colors.
+        """
+        self.assertEqual(hsv360_to_hsvdistance((360, 100, 100)), (2*pi, 1, 1))
+        self.assertEqual(hsv360_to_hsvdistance((360, 10, 10)), (2*pi, .1, .1))
+        self.assertEqual(hsv360_to_hsvdistance((180, 10.05, 10)), (pi, .1005, .1))
+
 
