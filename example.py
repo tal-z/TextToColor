db0fb7851e74eff678e33f57a1a484e978652951
Tal Zaken <talzaken@gmail.com>
2021-11-01 17:15:31 -0400

    readme updates

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 
 from ColorController import ColorController
 
-color = ColorController(name='forest')
+color = ColorController(name='cocoa')
 
 color.show_color()
 
-color.darken_color()
+color.darken_color(.3)
 
 color.show_color()
